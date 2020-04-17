67c8e5faf80c3377ae74d66cd12d2f1ee1c1880a
Jonathan Thom <jonathan.thom1990@gmail.com>
2020-04-17 13:00:34 -0700

    Tests for secure.Encrypt

--- secure/secure_test.go
+++ secure/secure_test.go
@@ -1,1 +1,42 @@
 package secure
+
+import (
+	"os"
+	"testing"
+)
+
+func TestEncrypt(t *testing.T) {
+	tests := []struct {
+		input       string
+		quackword   string
+		expectedLen int
+	}{
+		{
+			input:       "foo",
+			quackword:   "exists",
+			expectedLen: 44,
+		},
+		{
+			input:       "foo",
+			quackword:   "",
+			expectedLen: 0,
+		},
+	}
+
+	for i := 0; i < len(tests); i++ {
+		test := tests[i]
+		input := test.input
+		expectedLen := test.expectedLen
+		os.Setenv("QUACKWORD", test.quackword)
+
+		actual, _ := Encrypt(input)
+		actualLen := len(actual)
+
+		if actualLen != expectedLen {
+			t.Errorf("secure.Encrypt(%s) returned value of length %d, expected %d", input, actualLen, expectedLen)
+		}
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+}
