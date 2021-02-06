bb9008621a50ce53ce99999bde9ba9708f9a0fd6
Jonathan Thom <jonathanthom@hey.com>
2021-02-06 09:57:22 -0800

    Adds subtests to new_test

--- cmd/new_test.go
+++ cmd/new_test.go
@@ -11,12 +11,14 @@ func TestNew(t *testing.T) {
 	os.Setenv("QUACKWORD", "password")
 
 	var tests = []struct {
-		args     string
-		expected string
+		args        string
+		expected    string
+		description string
 	}{
 		{
-			args:     "valid entry",
-			expected: successMsg,
+			args:        "valid entry",
+			expected:    successMsg,
+			description: "when new entry is valid",
 		},
 		{
 			args: `
@@ -26,18 +28,20 @@ func TestNew(t *testing.T) {
 				morethan280charactersmorethan280charactersmorethan280characters
 				morethan280charactersmorethan280charactersmorethan280characters
 			`,
-			expected: tooManyCharsError,
+			expected:    tooManyCharsError,
+			description: "when new entry has too many characters",
 		},
 	}
 
-	for i := 0; i < len(tests); i++ {
-		test := tests[i]
-		args := strings.Split(test.args, " ")
-		expected := test.expected
-		actual := New(args...)
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			args := strings.Split(test.args, " ")
+			expected := test.expected
+			actual := New(args...)
 
-		if actual != expected {
-			t.Errorf("cmd.New(%v) returned %s, expected %s", args, actual, expected)
-		}
+			if actual != expected {
+				t.Errorf("cmd.New(%v) returned %s, expected %s", args, actual, expected)
+			}
+		})
 	}
 }
